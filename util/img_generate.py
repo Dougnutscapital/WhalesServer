c492285e180b2f05232e34a90e51cdb8a2280854
liangchg <lchn.guo@gmail.com>
2019-01-16 21:40:08 +0800

    fix csv

--- util/img_generate.py
+++ util/img_generate.py
@@ -121,8 +121,11 @@ def generate_sample(source_file, dst_file, pipeline):
     pl.imsave(dst_file, arr=res_im)
 
 
-def sample_generate(category, original_df, num_samples = 2):
-    df = pd.DataFrame(columns=["Image", "SrcImage", "Id"])
+df_columns = ["Image", "SrcImage", "Id"]
+
+
+def sample_generate(category, original_df, num_samples = 30):
+    df = pd.DataFrame(columns=df_columns)
     original_df_filtered = original_df[original_df["Id"] == category]
     for _ in range(0, num_samples):
         sample = original_df_filtered.sample(1).iloc[0]["Image"]
@@ -137,9 +140,9 @@ def sample_generate(category, original_df, num_samples = 2):
         # pipeline = pipeline[:num_policies]
         pipeline = generation_policies
         generate_sample(sample_file_path, sample_dst_file_path, pipeline)
-        elements = {'Image': sample_dst, "SrcImage": sample, "Id": category}
-        print(elements)
-        df.append(elements, ignore_index=True)
+        elem = pd.DataFrame([[sample_dst, sample, category]], columns=df_columns)
+        print(sample_dst, sample, category)
+        df = pd.concat([df, elem], ignore_index=True)
     return df
 
 
@@ -147,7 +150,7 @@ def main():
     df_train = pd.read_csv('../metadata/train.csv')
     df_sorted = df_train.groupby(['Id']).agg('count').sort_values("Image", ascending=False)
     print(df_sorted)
-    result_df = pd.DataFrame(columns=["Image", "SrcImage", "Id"])
+    result_df = pd.DataFrame(columns=df_columns)
     for idx in df_sorted.index:
         df = sample_generate(idx, df_train)
         result_df.append(df)
