58229b6f596898ea227b163f00bac6376317cc9c
liangchg <lchn.guo@gmail.com>
2019-01-16 14:56:39 +0800

    update img_generate

--- util/img_generate.py
+++ util/img_generate.py
@@ -1,5 +1,120 @@
+from skimage.transform import warp, AffineTransform, ProjectiveTransform
+from skimage.exposure import equalize_adapthist, equalize_hist, rescale_intensity, adjust_gamma, adjust_log, \
+    adjust_sigmoid
+from skimage.filters import gaussian
+from skimage.util import random_noise
+import random
+import pylab as pl
+import pandas as pd
+import numpy as np
+import os
+import cv2
+
+def randRange(a, b):
+    '''
+    a utility functio to generate random float values in desired range
+    '''
+    return pl.rand() * (b - a) + a
+
+
+def randomAffine(im):
+    '''
+    wrapper of Affine transformation with random scale, rotation, shear and translation parameters
+    '''
+    tform = AffineTransform(scale=(randRange(0.75, 1.3), randRange(0.75, 1.3)),
+                            rotation=randRange(-0.25, 0.25),
+                            shear=randRange(-0.2, 0.2),
+                            translation=(randRange(-im.shape[0] // 10, im.shape[0] // 10),
+                                         randRange(-im.shape[1] // 10, im.shape[1] // 10)))
+    return warp(im, tform.inverse, mode='reflect')
+
+
+def randomPerspective(im):
+    '''
+    wrapper of Projective (or perspective) transform, from 4 random points selected from 4 corners of the image within a defined region.
+    '''
+    region = 1 / 4
+    A = pl.array([[0, 0], [0, im.shape[0]], [im.shape[1], im.shape[0]], [im.shape[1], 0]])
+    B = pl.array([[int(randRange(0, im.shape[1] * region)), int(randRange(0, im.shape[0] * region))],
+                  [int(randRange(0, im.shape[1] * region)), int(randRange(im.shape[0] * (1 - region), im.shape[0]))],
+                  [int(randRange(im.shape[1] * (1 - region), im.shape[1])),
+                   int(randRange(im.shape[0] * (1 - region), im.shape[0]))],
+                  [int(randRange(im.shape[1] * (1 - region), im.shape[1])), int(randRange(0, im.shape[0] * region))],
+                  ])
+
+    pt = ProjectiveTransform()
+    pt.estimate(A, B)
+    return warp(im, pt, output_shape=im.shape[:2])
+
+
+def randomCrop(im):
+    '''
+    croping the image in the center from a random margin from the borders
+    '''
+    margin = 1 / 10
+    start = [int(randRange(0, im.shape[0] * margin)),
+             int(randRange(0, im.shape[1] * margin))]
+    end = [int(randRange(im.shape[0] * (1 - margin), im.shape[0])),
+           int(randRange(im.shape[1] * (1 - margin), im.shape[1]))]
+    return im[start[0]:end[0], start[1]:end[1]]
+
+
+def randomIntensity(im):
+    '''
+    rescales the intesity of the image to random interval of image intensity distribution
+    '''
+    return rescale_intensity(im,
+                             in_range=tuple(pl.percentile(im, (randRange(0, 10), randRange(90, 100)))),
+                             out_range=tuple(pl.percentile(im, (randRange(0, 10), randRange(90, 100)))))
+
+
+def randomGamma(im):
+    '''
+    Gamma filter for contrast adjustment with random gamma value.
+    '''
+    return adjust_gamma(im, gamma=randRange(0.5, 1.5))
+
+
+def randomGaussian(im):
+    '''
+    Gaussian filter for bluring the image with random variance.
+    '''
+    return gaussian(im, sigma=randRange(0, 5))
+
+
+def randomFilter(im):
+    '''
+    randomly selects an exposure filter from histogram equalizers, contrast adjustments, and intensity rescaler and applys it on the input image.
+    filters include: equalize_adapthist, equalize_hist, rescale_intensity, adjust_gamma, adjust_log, adjust_sigmoid, gaussian
+    '''
+    Filters = [equalize_adapthist, equalize_hist, adjust_log, adjust_sigmoid, randomGamma, randomGaussian,
+               randomIntensity]
+    filt = random.choice(Filters)
+    return filt(im)
+
+
+def randomNoise(im):
+    '''
+    random gaussian noise with random variance.
+    '''
+    var = randRange(0.001, 0.01)
+    return random_noise(im, var=var)
+
+
+def augment(im, steps):
+    '''
+    image augmentation by doing a sereis of transfomations on the image.
+    '''
+    for step in steps:
+        im = step(im)
+    return im
+
+
 
 def main():
+    df_train = pd.read_csv('../metadata/train.csv')
+    df_sorted = df_train.groupby(['Id']).agg('count').sort_values("Image", ascending=False)
+    print(df_sorted)
 
 
 if __name__ == '__main__':
